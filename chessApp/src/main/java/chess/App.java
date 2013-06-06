ab6f4c4d628426bb2def05b81b56d7b17990a41b
unknown <sudakshc@NOTEMUCHROCK-L7.y.corp.yahoo.com>
2013-06-06 19:29:19 +0530

    Testing commits on Git

--- chessApp/src/main/java/chess/App.java
+++ chessApp/src/main/java/chess/App.java
@@ -3,6 +3,7 @@ package chess;
 public class App {
 
 	public static void main(String[] args) {
+	//Test method
 		
 		ChessPieceServiceImpl impl = new ChessPieceServiceImpl();
 		impl.getShortestPath(ChessPiece.KNIGHT, ChessBoard.getInstance().getBoard()[0][0], ChessBoard.getInstance().getBoard()[1][0]);
